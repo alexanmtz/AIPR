4bc3fdc17ae33a7941c300db72a8496e44d2f31f
Alexandre Magno <alexanmtz@gmail.com>
2023-11-15 20:22:01 +0100

    debugging

--- aipr.py
+++ aipr.py
@@ -19,7 +19,6 @@ question = issue_title + "\n\n" + issue_body + "\n\n"
 def read_all_files_from_directory(directory):
     file_contents = {}
     for root, _, files in os.walk(directory):
-        print('directory', directory)
         for file in files:
             filepath = os.path.join(root, file)
             with open(filepath, 'r', encoding='utf-8', errors='ignore') as f:
@@ -64,13 +63,13 @@ if __name__ == "__main__":
 
     file_in_prompt = extract_specific_file_path(question)
     print('----------------------------------\n')
-    print('files in the repo', file_in_prompt)
+    print('files in prompt', file_in_prompt)
     print('----------------------------------\n')
     
     for filename, content in all_files.items():
         if filename in file_in_prompt:
+            print('filename: ', filename)
             modified_content = request_changes_from_openai(content)
-            print('content', content)
             print('modified content', modified_content)
             patch = generate_patch(content, modified_content, filename)
             patches[filename] = patch
